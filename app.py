9c72b374118a1ee1c78007a19f6dfb0a5bb06f83
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-01-13 17:18:39 +0100

    changed the order

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask import jsonify, make_response
+from collections import OrderedDict
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    data = {'statuscode':200,
-            'message':'my name is Marco'
-            }
+    data = OrderedDict([('code',200), ('message','my name is Marco')])
+
     return jsonify(data)
