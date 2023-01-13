de991a874934876c313d997c7e7744c21dac1fa0
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-01-13 20:40:13 +0100

    add info api

--- app.py
+++ app.py
@@ -7,5 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     data = OrderedDict([('code',200), ('message','my name is Marco')])
-
     return jsonify(data)
+
+
+@app.route('/info')
+def info():
+    data = OrderedDict([('code',200), ('message1','Web server delle api di Marco Vinciguerra')])
+    return jsonify(data)
+
+
