0beb7bbf8572c406079adbb49841982b1541b6e7
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-01-13 23:28:00 +0100

    array handling

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
-from flask import jsonify, make_response
+from flask import jsonify, make_response, request
 from collections import OrderedDict
 
 app = Flask(__name__)
+
+array = [1,2,3]
 
 @app.route('/')
 def hello_world():
@@ -15,4 +17,15 @@ def info():
     data = OrderedDict([('code',200), ('message1','Web server delle api di Marco Vinciguerra')])
     return jsonify(data)
 
+@app.route('/array')
+def getArray():
+    data = {"code": 200, "message": array}
+    return jsonify(data)
+
+@app.route('/add', methods=['POST'])
+def add_element():
+    element = request.json.get('element')
+    array.append(element)
+    data = {"code": 200, "message": "Array inserito con successo"}    
+    return jsonify(data)
 
