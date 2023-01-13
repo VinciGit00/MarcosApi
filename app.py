039a88401857db4236775585881045dd04c717ec
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-01-13 16:32:50 +0100

    add the correct libraries

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask import jsonify, make_response
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    data = {'statuscode':200,
+            'message':'my name is Marco'
+            }
     return 'My My name is Marcoo!'
