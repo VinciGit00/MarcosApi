b2c4cb610a4b9793620ea509e1628183e83072be
Marco Vinciguerra <88108002+VinciGit00@users.noreply.github.com>
2023-01-14 10:55:17 +0100

    add Person created

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from collections import OrderedDict
 
 app = Flask(__name__)
 
-array = [1,2,3]
+array = [1, 2, 3]
+
+person = [{"name" : "Marco", "surname" : "Vinciguerra"}, {"name" : "Gabriele", "surname" : "Marchesi"}]
+
 
 @app.route('/')
 def hello_world():
@@ -27,5 +30,17 @@ def add_element():
     element = request.json.get('element')
     array.append(element)
     data = {"code": 200, "message": "Array inserito con successo"}    
+    return jsonify(data)
+
+@app.route('/addPerson', methods=['POST']) 
+def add_person():
+    person_to_add = {"name":request.json.get('name'), "surname":request.json.get('surname')}
+    person.append(person_to_add)
+    data = {"code": 200, "message": "Persona inserita con successo"}
+    return jsonify(data)
+
+@app.route('/getPerson') 
+def get_person():
+    data = {"code": 200, "message":person}
     return jsonify(data)
 
